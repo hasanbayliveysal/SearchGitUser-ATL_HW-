68f89ab0777b758ad45ab322f7fcc0256ce21d23
Veysal Hasanbayov <hasanbayliveysaldev@gmail.com>
2024-05-23 20:19:11 +0400

    Update AppDelegate.swift

--- SearchGithubUser/Others/AppDelegate.swift
+++ SearchGithubUser/Others/AppDelegate.swift
@@ -10,8 +10,6 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         let vc = MainViewController()
@@ -19,7 +17,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         return true
     }
-
-  
 }
 
